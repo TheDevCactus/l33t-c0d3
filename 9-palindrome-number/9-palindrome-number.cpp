7a5a328955b7d22847c79ec683faaa136b6f891d
Morgan Brown <36177304+Morganb816@users.noreply.github.com>
2022-05-06 09:08:44 -0400

    Time: 20 ms (49.07%), Space: 5.9 MB (75.84%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+      if (x < 0) return false;
+      
+      long long int reversed = 0;
+      int t = x;
+      while (t > 0) {
+        reversed *= 10;
+        reversed += (t % 10);
+        t /= 10;
+      }
+      return x - reversed == 0;
+    }
+};
