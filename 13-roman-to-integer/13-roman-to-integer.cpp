b44a262b579b27c9c5acab1a4dd024a51111a851
Morgan Brown <36177304+Morganb816@users.noreply.github.com>
2022-05-06 13:29:42 -0400

    Time: 11 ms (70.65%), Space: 8.1 MB (30.83%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -1,15 +1,7 @@
 class Solution {
 public:
     int romanToInt(string s) {
-      map<char, int> conversionTable {
-        {'I',1},
-        {'V',5},
-        {'X',10},
-        {'L',50},
-        {'C',100},
-        {'D',500},
-        {'M',1000}
-      };
+      map<char, int> conversionTable { {'I',1}, {'V',5}, {'X',10}, {'L',50}, {'C',100}, {'D',500}, {'M',1000} };
       int out = 0;
       for (uint i = 0; i < s.length(); i ++) {
         if (i + 1 < s.length()) {
