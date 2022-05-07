b3bdcbe693184bcaa0f0c2d5aeb3c063b13327ad
Morgan Brown <36177304+Morganb816@users.noreply.github.com>
2022-05-07 08:56:10 -0400

    Time: 3 ms (63.34%), Space: 8.8 MB (73.91%) - LeetHub

---
+++ 27-remove-element/27-remove-element.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+      unsigned char l = 0, h = 0;
+      while (h < nums.size()) {
+        if (nums[h] != val) {
+          nums[l] = nums[h];
+          l++;
+        }
+        h++;
+      }
+      return l;
+    }
+};
