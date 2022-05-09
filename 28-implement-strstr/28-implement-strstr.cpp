e6ba46df4d8e3ad7afdb00c8abd23da7925701b8
Morgan Brown <36177304+Morganb816@users.noreply.github.com>
2022-05-09 12:57:53 -0400

    Time: 3 ms (64.92%), Space: 6.3 MB (77.28%) - LeetHub

---
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if (needle.length() == 0) return 0;
+        int l = 0, h = 0;
+        while (h < haystack.length()) {
+          h++;
+          if (h - l != needle.length()) continue;
+          bool foundMatch = true;
+          for (uint i = 0; i < needle.length(); i ++) {
+            if (haystack[l + i] == needle[i]) continue;
+            foundMatch = false;
+          }
+          if (foundMatch) return l;
+          l++;
+        }
+        return -1;
+    }
+};
